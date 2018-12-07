c8dfd78af5b8e3e8f3b437e87efc56b62c70bb8c
Kseniya Petukhova <kkpetukhova@gmail.com>
2018-12-07 23:38:52 +0100

    Buttons, token in a separate file, comment section

--- chgk.py
+++ chgk.py
@@ -15,7 +15,10 @@ import requests
 from urllib.parse import quote
 import xml.etree.ElementTree as ET
 
-bot = telebot.TeleBot("")
+with open('token.txt','r') as output:
+    token = output.read()
+    bot = telebot.TeleBot(str(token).strip())
+
 current_shown_dates={}
 
 iter_q = 0
@@ -25,47 +28,52 @@ def bot_start(message):
     now = datetime.datetime.now() #Current date
     bot.send_message(message.chat.id, "Чтобы начать - напиши тему\n/next : следующий вопрос\n/answer : ответ на текущий вопрос\nВопросы взяты из Базы вопросов «Что? Где? Когда?» \nhttps://db.chgk.info")
 
+def PrintChoice(mode):
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    if (mode == 'q'):
+        answer_button = types.KeyboardButton(text="Answer")
+        next_button = types.KeyboardButton(text="Next question")
+    if (mode == 'a'):
+        answer_button = types.KeyboardButton(text="Comment")
+        next_button = types.KeyboardButton(text="Next question")
+    keyboard.add(answer_button, next_button)
+    return keyboard
 
-@bot.message_handler(regexp="^(?!\/)[\w]+")
-def ChooseTheme(message):
-    theme = message.text
+@bot.message_handler(regexp="Next question")
+def PrintQuestion(message):
     global iter_q
-    url = 'https://db.chgk.info/xml/search/questions/' + quote(theme) + '/types123/QC'
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        with open('question.xml','wb') as output:
-            for line in response: # files are iterable
-                output.write(line)
-    tree = ET.parse('question.xml')
-    for node in tree.iter('question'):
-        for elem in node.iter():
-            if not elem.tag==node.tag:
-                if (elem.tag == 'Question'):
-                    bot.send_message(message.chat.id, "{}: {}".format(elem.tag, elem.text))
-                    iter_q = 1
-            if (iter_q == 1):
-                break
-        if (iter_q == 1):
-            break
-
-@bot.message_handler(commands=['next'])
-def NextQuestion(message):
-    global iter_q
+    keyboard = PrintChoice('q')
     tmp = 0
-    tree = ET.parse('question.xml')
     i = 0
+    tree = ET.parse('question.xml')
     for node in tree.iter('question'):
         for elem in node.iter():
             if (not elem.tag==node.tag) and (elem.tag == 'Question'):
                 if i == iter_q:
-                    bot.send_message(message.chat.id, "{}: {}".format(elem.tag, elem.text))
+                    bot.send_message(message.chat.id, text = "{}: {}".format(elem.tag, elem.text), reply_markup=keyboard)
                     iter_q = iter_q + 1
                     tmp = 1
                 i = i + 1
             if (tmp == 1): break
 
-@bot.message_handler(commands=['answer'])
-def NextQuestion(message):
+@bot.message_handler(regexp="Comment")
+def PrintAnswer(message):
+    global iter_q
+    tree = ET.parse('question.xml')
+    i = 0
+    tmp = 0
+    for node in tree.iter('question'):
+        for elem in node.iter():
+            if (not elem.tag==node.tag) and (elem.tag == 'Comments'):
+                if i == iter_q - 1:
+                    keyboard = PrintChoice('a')
+                    bot.send_message(message.chat.id, text = "{}: {}".format(elem.tag, elem.text), reply_markup=keyboard)
+                    tmp = 1
+                i = i + 1
+            if (tmp == 1): break
+
+@bot.message_handler(regexp="Answer")
+def PrintAnswer(message):
     global iter_q
     tree = ET.parse('question.xml')
     i = 0
@@ -74,10 +82,36 @@ def NextQuestion(message):
         for elem in node.iter():
             if (not elem.tag==node.tag) and (elem.tag == 'Answer'):
                 if i == iter_q - 1:
-                    bot.send_message(message.chat.id, "{}: {}".format(elem.tag, elem.text))
+                    keyboard = PrintChoice('a')
+                    bot.send_message(message.chat.id, text = "{}: {}".format(elem.tag, elem.text), reply_markup=keyboard)
+                    #bot.send_message(ci, "------------", reply_markup=keyboard)
                     tmp = 1
                 i = i + 1
-        if (tmp == 1): break
+            if (tmp == 1): break
+
+@bot.message_handler(regexp="^(?!\/)[\w]+")
+def ChooseTheme(message):
+    theme = message.text
+    url = 'https://db.chgk.info/xml/search/questions/' + quote(theme) + '/types123/QC'
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        with open('question.xml','wb') as output:
+            for line in response: # files are iterable
+                output.write(line)
+    global iter_q
+    keyboard = PrintChoice('q')
+    tmp = 0
+    i = 0
+    tree = ET.parse('question.xml')
+    for node in tree.iter('question'):
+        for elem in node.iter():
+            if (not elem.tag==node.tag) and (elem.tag == 'Question'):
+                if i == iter_q:
+                    bot.send_message(message.chat.id, "{}: {}".format(elem.tag, elem.text), reply_markup=keyboard)
+                    iter_q = iter_q + 1
+                    tmp = 1
+                i = i + 1
+            if (tmp == 1): break
 
 @bot.message_handler(commands=['date'])
 def check_date(message):
@@ -89,9 +123,9 @@ def check_date(message):
 #    bot.reply_to(message, message.text)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'ignore')
-def ignore(call):
-    bot.answer_callback_query(call.id, text="")
+#@bot.callback_query_handler(func=lambda call: call.data == 'ignore')
+#def ignore(call):
+#   bot.answer_callback_query(call.id, text="")
 
 
 bot.polling()
